18e3c6a51d2bae737f1072bf0e7c2dfc05f931bd
franco <francobof01@gmail.com>
2022-04-10 19:31:39 -0300

    practica 2 ejer10

--- practica2/ejercicio10.py
+++ practica2/ejercicio10.py
@@ -9,6 +9,10 @@ eval2= arch_eval2.read().replace(',', "").replace('\'',"").split()
 estudiantes=[]
 total= 0
 
+for i in range(0,len(nombres)):
+     estudiante= {"nombre": nombres[i], "nota": int(eval1[i]) + int(eval2[i]) }
+     estudiantes.append(estudiante)
+     total+= estudiante["nota"]
 #for n in nombre:
 #    for nota1 in eval1:
 #        for nota2 in eval2:
@@ -16,10 +20,10 @@ total= 0
 #            estudiantes.append(estudiante)
 #            total+= estudiante["nota evaluacion 1 y evaluacion 2"]
 
-for nombre, nota1, nota2 in zip(nombres, eval1, eval2):
-      estudiante = {"nombre": nombre, "nota": int(nota1) + int(nota2)}
-      estudiantes.append(estudiante)
-      total += estudiante["nota"]
+#for nombre, nota1, nota2 in zip(nombres, eval1, eval2):
+#      estudiante = {"nombre": nombre, "nota": int(nota1) + int(nota2)}
+#      estudiantes.append(estudiante)
+#      total += estudiante["nota"]
 
 promedio= total  / len (estudiantes)
 print(promedio)
